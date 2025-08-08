862c91d95cf33c20f4abb0ddd5085c31ffcf618e
LSH005 <isihyeon005shingu@gmail.com>
2025-08-08 21:51:13 +0900

    [feat] 노드 생성, 패스파인딩 구현

---
+++ Assets/Pathfinding/Script/NodeMovement.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class NodeMovement : MonoBehaviour
+{
+    public int id;
+    public bool parentalAuthority = true;
+    public bool isRoad = false;
+    public float nodeSpacing = 1f;
+    public GameObject newNode;
+
+    private BoxCollider2D nodeCollider;
+    private readonly Vector2[] directions = new Vector2[]
+    {
+        Vector2.up,
+        Vector2.right,
+        Vector2.down,
+        Vector2.left
+    };
+
+    private void Awake()
+    {
+        nodeCollider = GetComponent<BoxCollider2D>();
+        if (nodeCollider == null)
+        {
+            Debug.LogError("BoxCollider2D 없음");
+        }
+
+        PathfindManager.allQualifiedNodes.Add(this);
+    }
+
+    public void PermissionsUpdate()
+    {
+        if (nodeCollider == null)
+        {
+            return;
+        }
+
+        Vector2 searchSize = nodeCollider.size;
+
+        bool hasEmptySpace = false;
+
+        foreach (Vector2 direction in directions)
+        {
+            Vector2 searchPosition = (Vector2)transform.position + (direction * nodeSpacing);
+            Collider2D[] colliders = Physics2D.OverlapBoxAll(searchPosition, searchSize, 0f);
+
+            bool foundObstacleOrNode = false;
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.gameObject == this.gameObject)
+                {
+                    continue;
+                }
+
+                if (collider.CompareTag("obstacle") || collider.CompareTag("Node"))
+                {
+                    foundObstacleOrNode = true;
+                    break;
+                }
+            }
+
+            if (!foundObstacleOrNode)
+            {
+                hasEmptySpace = true;
+                break;
+            }
+        }
+
+        parentalAuthority = hasEmptySpace;
+        if (!parentalAuthority)
+        {
+            PathfindManager.allQualifiedNodes.Remove(this);
+        }
+    }
+
+    public void SummonNode()
+    {
+        foreach (Vector2 direction in directions)
+        {
+            Vector2 spawnPosition = (Vector2)transform.position + (direction * nodeSpacing);
+
+            Collider2D[] colliders = Physics2D.OverlapBoxAll(spawnPosition, nodeCollider.size, 0f);
+
+            bool foundObstacleOrNode = false;
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.gameObject != this.gameObject && (collider.CompareTag("obstacle") || collider.CompareTag("Node")))
+                {
+                    foundObstacleOrNode = true;
+                    break;
+                }
+            }
+
+            if (!foundObstacleOrNode)
+            {
+                GameObject newGameObject = Instantiate(newNode, spawnPosition, Quaternion.identity);
+
+                NodeMovement newNodeScript = newGameObject.GetComponent<NodeMovement>();
+                if (newNodeScript != null)
+                {
+                    newNodeScript.id = PathfindManager.nodeId;
+                    // PathfindManager.nodeId++;
+                    // 정적 변수로 변경함에 따라 이곳에서 id가 바뀌지 않음
+                }
+            }
+        }
+
+        isRoad = true;
+    }
+
+    private void OnDestroy()
+    {
+        PathfindManager.allQualifiedNodes.Remove(this);
+    }
+}
--- Assets/Pathfinding/Script/PathfindManager.cs
+++ Assets/Pathfinding/Script/PathfindManager.cs
@@ -1,16 +1,28 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PrefabSpawner : MonoBehaviour
+public class PathfindManager : MonoBehaviour
 {
-    public GameObject walls;
+    public GameObject obstacle;
+    public GameObject node;
+    public readonly int maxAttempts = 500;
+    public Vector2 targetPos;
 
-    private int nodeNumber = 0;
+    public static int nodeId = 1;
+    public static List<NodeMovement> allQualifiedNodes = new List<NodeMovement>();
 
+    private NodeMovement parentNode;
     private bool isActivating = false;
+
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Pathfinding");
+        }
+
         if (isActivating)
         {
 
@@ -21,6 +33,82 @@ public class PrefabSpawner : MonoBehaviour
             {
                 isActivating = true;
 
+                GameObject trackerObject = GameObject.FindWithTag("Tracker");
+                if (trackerObject != null)
+                {
+                    GameObject newNode = Instantiate(node, trackerObject.transform.position, Quaternion.identity);
+
+                    NodeMovement nodeMovementScript = newNode.GetComponent<NodeMovement>();
+                    if (nodeMovementScript != null)
+                    {
+                        nodeMovementScript.id = nodeId;
+                        nodeId++;
+                    }
+                    else
+                    {
+                        Debug.LogError("프리팹에 NodeMovement 스크립트 없음");
+                    }
+
+                }
+                else
+                {
+                    Debug.LogError("Tracker 태그를 가진 오브젝트 없음");
+                }
+
+                GameObject targetObject = GameObject.FindWithTag("Target");
+                if (targetObject != null)
+                {
+                    targetPos = targetObject.transform.position;
+                }
+                else
+                {
+                    Debug.LogError("Target 태그를 가진 오브젝트 없음");
+                }
+
+                int attempts = 0;
+                while (attempts <= maxAttempts)
+                {
+                    attempts++;
+
+                    if (allQualifiedNodes.Count == 0)
+                    {
+                        Debug.LogError("허용된 부모 노드 없음");
+                        break;
+                    }
+
+                    for (int i = allQualifiedNodes.Count - 1; i >= 0; i--)
+                    {
+                        NodeMovement node = allQualifiedNodes[i];
+                        if (node != null)
+                        {
+                            node.PermissionsUpdate();
+                        }
+                    }
+
+                    if (allQualifiedNodes.Count == 0) break;
+
+                    FindClosestNodeToTarget();
+                    if (parentNode != null)
+                    {
+                        parentNode.SummonNode();
+                        nodeId++;
+
+                        float distance = Vector2.Distance(parentNode.transform.position, targetPos);
+                        if (distance <= parentNode.nodeSpacing)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                NodeMovement[] allNodes = FindObjectsOfType<NodeMovement>();
+                foreach (NodeMovement node in allNodes)
+                {
+                    if (!node.isRoad)
+                    {
+                        Destroy(node.gameObject);
+                    }
+                }
             }
 
             if (Input.GetMouseButton(0))
@@ -28,7 +116,33 @@ public class PrefabSpawner : MonoBehaviour
                 Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 mousePosition.z = 0f;
 
-                Instantiate(walls, mousePosition, Quaternion.identity);
+                Instantiate(obstacle, mousePosition, Quaternion.identity);
+            }
+        }
+    }
+
+    private void FindClosestNodeToTarget()
+    {
+        if (allQualifiedNodes.Count == 0)
+        {
+            Debug.LogWarning("노드 없음");
+            return;
+        }
+
+        float closestDistance = Mathf.Infinity;
+        parentNode = null;
+
+        foreach (NodeMovement node in allQualifiedNodes)
+        {
+            if (node != null)
+            {
+                float distance = Vector2.Distance(node.transform.position, targetPos);
+
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    parentNode = node;
+                }
             }
         }
     }
