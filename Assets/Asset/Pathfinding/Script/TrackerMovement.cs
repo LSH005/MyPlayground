95bd4034cc0bc8a19b94ab4ba28cdb50ba3368e4
LSH005 <isihyeon005shingu@gmail.com>
2025-08-12 01:03:29 +0900

    [fix] 디버그 로그가 잘못된 정보를 표시하는 오류 수정

--- Assets/Asset/Pathfinding/Script/TrackerMovement.cs
+++ Assets/Asset/Pathfinding/Script/TrackerMovement.cs
@@ -151,7 +151,7 @@ public class TrackerMovement : MonoBehaviour
 
         if (!isObstaclesFound)
         {
-            Debug.Log($"{trackingNodeIndex}번 길 (ID {allRoadNodes[trackingNodeIndex].id}) 노드에서 {trackingNodeIndex+1}번 길 (ID {allRoadNodes[trackingNodeIndex+1].id}) 노드로 가는 도중 더 좋은 직선을 찾음.");
+            Debug.Log($"{trackingNodeIndex-1}번 길 (ID {allRoadNodes[trackingNodeIndex-1].id}) 노드에서 {trackingNodeIndex}번 길 (ID {allRoadNodes[trackingNodeIndex].id}) 노드로 가는 도중 {trackingNodeIndex+1}번 길 (ID {allRoadNodes[trackingNodeIndex+1].id}) 노드로 가는 직선을 찾음.");
 
             UpdateNextTarget();
         }
