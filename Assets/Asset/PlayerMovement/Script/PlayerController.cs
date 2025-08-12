f4fbfd23de0c46b3dcd5a8db2a46e5ecc7708c1c
LSH005 <isihyeon005shingu@gmail.com>
2025-08-12 21:21:40 +0900

    [feat] PlayerMovement에서 코요테 타임 구현

--- Assets/Asset/PlayerMovement/Script/PlayerController.cs
+++ Assets/Asset/PlayerMovement/Script/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
 
     public float jumpForce = 15f; // 점프 힘
+    public float coyoteTime = 0.25f; // 코요테 타임
     public Transform groundCheck; // 바닥을 감지할 위치
     public LayerMask groundLayer; // 바닥으로 인식할 레이어
     public float groundCheckRadius = 0.2f; // 바닥 감지 원의 반지름
 
     private float moveInput;
+    public float coyote;
     private bool isGrounded;
+    private bool isAfterCoyoteTime;
     private bool isFacingRight = true;
     private Rigidbody2D rb;
 
@@ -25,10 +28,27 @@ public class PlayerController : MonoBehaviour
         moveInput = Input.GetAxisRaw("Horizontal");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (isGrounded)
+        {
+            coyote = 0f;
+            isAfterCoyoteTime = false;
+        }
+        else if (coyote <= coyoteTime)
+        {
+            coyote += Time.deltaTime;
+            if (coyote >= coyoteTime)
+            {
+                isAfterCoyoteTime = true;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space) && !isAfterCoyoteTime)
         {
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+            coyote = coyoteTime;
+            isAfterCoyoteTime = true;
         }
 
         if (moveInput > 0 && !isFacingRight)
