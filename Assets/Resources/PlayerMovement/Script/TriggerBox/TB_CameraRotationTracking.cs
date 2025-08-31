0a317c79c1c4f2e7730ce247cb35f92e4949c2d7
LSH005 <isihyeon005shingu@gmail.com>
2025-08-31 23:23:21 +0900

    [feat] RotationTracking 트리거박스 파츠에 설정 추가

--- Assets/Resources/PlayerMovement/Script/TriggerBox/TB_CameraRotationTracking.cs
+++ Assets/Resources/PlayerMovement/Script/TriggerBox/TB_CameraRotationTracking.cs
@@ -5,11 +5,42 @@ public class TB_CameraRotationTracking : MonoBehaviour
     [Header("태그")]
     public string compareTag = "Player";
 
+    [Header("추적할 태그 / 오프셋")]
+    public string trackTargetTag = "Player";
+    public Vector3 offset;
+
+    [Header("부가 설정")]
+    [Tooltip("Scene이 시작할 때, 추적할 태그를 가진 오브젝트를 탐색하는 데에 실패한다면\n작동할 때 한번 더 탐색한다.\n재탐색 이후에도 오브젝트가 없다면 작동 중지한다.")]
+    public bool shouldRetryFinding = true;
+
+    private GameObject trackTargetObj;
+
+    private void Start()
+    {
+        trackTargetObj = GameObject.FindWithTag(trackTargetTag);
+
+        if (trackTargetObj == null && !shouldRetryFinding)
+        {
+            this.enabled = false;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(compareTag))
         {
-            CameraMovement.RotationTracking(other.transform, Vector3.zero);
+            if (trackTargetObj == null)
+            {
+                trackTargetObj = GameObject.FindWithTag(trackTargetTag);
+
+                if (trackTargetObj == null)
+                {
+                    this.enabled = false;
+                    return;
+                }
+            }
+
+            CameraMovement.RotationTracking(trackTargetObj.transform, offset);
         }
     }
 }
