48ae1b0204a30fbf8981d596049858ae7dcf4091
LSH005 <isihyeon005shingu@gmail.com>
2025-09-05 22:21:56 +0900

    [feat] 말풍선 프리팹, 말풍선 조작 스크립트 구현

--- Assets/Resources/PlayerMovement/Script/DialogueBox/DialogueBoxMain.cs
+++ Assets/Resources/PlayerMovement/Script/DialogueBox/DialogueBoxMain.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource), typeof(BoxCollider2D))]
 public class DialogueBoxMain : MonoBehaviour
 {
+    [Header("플레이어 태그")]
+    public string playerTag = "Player";
+
+
     [Header("대화 말풍선")]
     public GameObject keyMarker;
     public GameObject dialogueBubble;
@@ -12,13 +16,43 @@ public class DialogueBoxMain : MonoBehaviour
     public bool isMultiConversation = true;     // 여러 번 작동시켜 Dialogue 배열을 순차적으로 재생시킬지에 대한 여부.
 
     private bool isDialogueActive = false;
+    private bool isTyping = false;
     private GameObject dialogueBubbleInstance;
+
+    
+
+    private void Awake()
+    {
+        keyMarker.SetActive(false);
+
+        BoxCollider2D boxCol = GetComponent<BoxCollider2D>();
+        boxCol.isTrigger = true;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        keyMarker.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        keyMarker.SetActive(false);
+    }
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (other.tag == playerTag)
         {
+            if (!isDialogueActive)
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
 
+                }
+            }
         }
     }
+
+
 }
--- Assets/Resources/PlayerMovement/Script/DialogueBubbleReference.cs
+++ Assets/Resources/PlayerMovement/Script/DialogueBubbleReference.cs
@@ -3,5 +3,56 @@ using UnityEngine;
 
 public class DialogueBubbleReference : MonoBehaviour
 {
-    public TextMeshPro Text;
+    public TextMeshPro dialogueText;
+    public GameObject bubbleBody;
+    public GameObject upperTail;
+    public GameObject lowerTail;
+
+    public float bubbleOffsetLimit = 1.0f;
+
+
+    public void SetTailToLower(bool isLower)
+    {
+        if (isLower)
+        {
+            upperTail.SetActive(false);
+            lowerTail.SetActive(true);
+        }
+        else
+        {
+            upperTail.SetActive(true);
+            lowerTail.SetActive(false);
+        }
+    }
+
+    public void SetBubbleOffset(float offsetDistance)
+    {
+        if (bubbleOffsetLimit == 0.0f)
+        {
+            bubbleBody.transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            if (bubbleOffsetLimit < 0)
+            {
+                bubbleOffsetLimit = Mathf.Abs(bubbleOffsetLimit);
+            }
+
+            bubbleBody.transform.localPosition = new Vector3(
+                Mathf.Clamp(offsetDistance, -bubbleOffsetLimit, bubbleOffsetLimit),
+                0,
+                0
+            );
+        }
+    }
+
+    public void SetText(string text)
+    {
+        dialogueText.text = text;
+    }
+
+    public void SetPosition(Vector3 targetPosition)
+    {
+        transform.position = targetPosition;
+    }
 }
