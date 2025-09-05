56841c7c7ed8fe9117d388cc43ee3972c23049b4
LSH005 <isihyeon005shingu@gmail.com>
2025-09-06 04:17:22 +0900

    [feat] 채팅창 출력 구현

--- Assets/Resources/PlayerMovement/Script/DialogueBox/DialogueBoxMain.cs
+++ Assets/Resources/PlayerMovement/Script/DialogueBox/DialogueBoxMain.cs
@@ -22,6 +22,8 @@ public class DialogueBoxMain : MonoBehaviour
     private bool isTyping = false;
     private GameObject dialogueBubbleInstance;
     private Dialogue currentDialogue;
+    private DialogueBubbleReference bubbleScript;
+    private AudioSource AudioSource;
 
 
     private void Awake()
@@ -30,6 +32,7 @@ public class DialogueBoxMain : MonoBehaviour
 
         BoxCollider2D boxCol = GetComponent<BoxCollider2D>();
         boxCol.isTrigger = true;
+        AudioSource = GetComponent<AudioSource>();
     }
 
 
@@ -61,32 +64,92 @@ public class DialogueBoxMain : MonoBehaviour
 
     private void StartDialogue()
     {
+        if (dialogueBubble != null)
+        {
+            GameObject newBubble = Instantiate(dialogueBubble);
+            bubbleScript = newBubble.GetComponent<DialogueBubbleReference>();
+
+            if (bubbleScript == null)
+            {
+                Debug.LogError("할당된 프리팹에 DialogueBubbleReference 스크립트 없음.");
+                return;
+            }
+        }
+        else Debug.LogError("말풍선 프리팹 할당되지 않음.");
+
         if (Dialogue != null)
         {
-            isDialogueActive = true;
-            currentDialogue = Dialogue[DialogueIndex];
-            StartCoroutine(TypeSentenceCoroutine());
+            if (DialogueIndex >= 0 && DialogueIndex < Dialogue.Length)
+            {
+                if (Dialogue[DialogueIndex] != null)
+                {
+                    isDialogueActive = true;
+                    currentDialogue = Dialogue[DialogueIndex];
+                    StartCoroutine(TypeSentenceCoroutine());
+                }
+                else Debug.LogError("첫 인덱스가 범위를 벗어났거나, 내용물이 비어 있음.");
+            }
+            else Debug.LogError("배열의 유효한 범위를 벗어남.");
         }
-        else
-        {
-            Debug.LogError("할당된 SO가 없음");
-        }
+        else Debug.LogError("할당된 SO가 없음");
     }
 
     public IEnumerator TypeSentenceCoroutine()
     {
-        int sectionIndex = 0;
-        DialogueLine dialogueSection = currentDialogue.lines[sectionIndex];
-        
-        yield return null;
-
-        if (dialogueSection.sentence != null)
+        for (int i = 0; i < currentDialogue.section.Length; i++)  // 섹션 전체 반복
         {
+            DialogueLine dialogueSection = currentDialogue.section[i];
 
-        }
-        else
-        {
-            Debug.LogError("할당된 SO에 문자열 배열 없음");
+            bubbleScript.transform.position = dialogueSection.bubblePosition;
+            bubbleScript.SetBubbleOffset(dialogueSection.bubbleBodyOffset);
+            bubbleScript.SetTailToLower(dialogueSection.isTailDown);
+            bubbleScript.SetText("");
+
+            if (dialogueSection.sentence != null)   // 섹션 문자열 배열 유효성 검사 1
+            {
+                if (dialogueSection.sentence.Length > 0)    // 섹션 문자열 배열 유효성 검사 2
+                {
+                    for (int j = 0; j < dialogueSection.sentence.Length; j++)   // 섹션 내부 문자열 반복
+                    {
+                        if (!string.IsNullOrEmpty(dialogueSection.sentence[j])) // 섹션 문자열 배열 유효성 검사 3
+                        {
+                            string fullDialogueText = dialogueSection.sentence[j];
+                            string DialogueText = "";
+                            bool hasSound = dialogueSection.typingSound != null;
+
+                            float timer = 0f;
+                            int charIndex = 0;
+
+                            // F키를 누르면 다음 문장으로 넘어가는 기능
+                            while (charIndex < fullDialogueText.Length)
+                            {
+                                yield return null;
+                                if (Input.GetKeyDown(KeyCode.F))
+                                {
+                                    bubbleScript.SetText(fullDialogueText);
+                                    break;
+                                }
+
+                                if (timer >= dialogueSection.intervalTime)
+                                {
+                                    DialogueText += fullDialogueText[charIndex];
+                                    bubbleScript.SetText(DialogueText);
+                                    if (hasSound) AudioSource.PlayOneShot(dialogueSection.typingSound);
+                                    charIndex++;
+                                    timer = 0f;
+                                }
+
+                                timer += Time.deltaTime;
+                            }
+                            yield return null;
+                            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.F));
+                        }
+                        else Debug.LogError("문자열이 비었음");
+                    }
+                }
+                else Debug.LogError("배열이 비었음");
+            }
+            else Debug.LogError("할당된 SO에 배열 자체가 없음");
         }
     }
 }
--- Assets/Resources/PlayerMovement/Script/SO/Script/Dialogue.cs
+++ Assets/Resources/PlayerMovement/Script/SO/Script/Dialogue.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 [System.Serializable]
 public class DialogueLine
 {
+    [Header("말풍선 위치")]
     public Vector3 bubblePosition;  // 말풍선의 위치. 원점 (0,0,0) 이라면 움직이지 않음.
+    public float bubbleBodyOffset;  // 말풍선 몸통의 좌-우 오프셋
+    public bool isTailDown;   // 말풍선 꼬리표가 위쪽에 있을지에 대한 여부
 
     [TextArea(1, 10)]   // 인스펙터 창에서 표시되는 입력란 열 최소, 최대 크기
     public string[] sentence;    // 대사 내용 배열 (입력되지 않으면 문자열을 넘기거나, 다음 세션으로 넘어감)
@@ -18,5 +21,5 @@ public class DialogueLine
 [CreateAssetMenu(fileName = "Totally Awesome Dialogue", menuName = "Dialogue/New Dialogue")]
 public class Dialogue : ScriptableObject
 {
-    public DialogueLine[] lines;
+    public DialogueLine[] section;
 }
